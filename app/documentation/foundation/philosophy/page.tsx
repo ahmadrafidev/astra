d92e7da48868d6735b12aed080b6e07d3224ea3b
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-06-02 00:08:02 +0700

    add link to card in docs page

--- app/documentation/foundation/philosophy/page.tsx
+++ app/documentation/foundation/philosophy/page.tsx
@@ -15,23 +15,22 @@ const PhilosophyPage: React.FC = () => {
                 <p className="text-sm md:text-base">
                     Astra UI is driven by a set of core principles aimed at making web development more consistent, efficient, and accessible. The philosophy of Astra UI revolves around the following key tenets:
                 </p>
-                <h2 className="text-lg font-semibold">Consistency</h2>
+                <h2 className="text-lg font-semibold">1. Consistency</h2>
                 <p className="text-sm md:text-base">
                     I believe in maintaining a unified design language across all components. This ensures a cohesive look and feel, enhancing the overall user experience. Consistency is key to reducing cognitive load and creating intuitive interfaces.
                 </p>
-                <h2 className="text-lg font-semibold">Reusability</h2>
+                <h2 className="text-lg font-semibold">2. Reusability</h2>
                 <p className="text-sm md:text-base">
                     Components should be designed to be reusable across different projects. By creating modular and flexible components, I aim to reduce redundant code and streamline the development process.
                 </p>
-                <h2 className="text-lg font-semibold">Accessibility</h2>
+                <h2 className="text-lg font-semibold">3. Accessibility</h2>
                 <p className="text-sm md:text-base">
                     Accessibility is at the forefront of my design philosophy. I strive to create components that are usable by everyone, including individuals with disabilities. Ensuring that my components meet accessibility standards is a top priority.
                 </p>
-                <h2 className="text-lg font-semibold">Customizability</h2>
+                <h2 className="text-lg font-semibold">4. Customizability</h2>
                 <p className="text-sm md:text-base">
                     I understand that every project has unique needs. My components are designed to be easily customizable, allowing developers to tailor them to fit their specific requirements and branding guidelines.
                 </p>
-
             </div>
         </Layout>
     );
--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -25,10 +25,12 @@ const Page: React.FC = () => {
                             <Link href={item.path} className="text-gray-900 dark:text-gray-50">
                                 {item.name}
                             </Link>
-                            <p className="text-sm text-gray-900 dark:text-gray-100 mt-2 leading-loose">
-                                {item.name === 'About' && 'Learn more about Astra UI, its purpose, and the principles that guide its design and development.'}
-                                {item.name === 'Philosophy' && 'Discover the core philosophy behind Astra UI and how it aims to make development easier and more accessible.'}
-                            </p>
+                            <Link href={item.path}>
+                                <p className="text-sm text-gray-900 dark:text-gray-100 mt-2 leading-loose">
+                                    {item.name === 'About' && 'Learn more about Astra UI, its purpose, and the principles that guide its design and development.'}
+                                    {item.name === 'Philosophy' && 'Discover the core philosophy behind Astra UI and how it aims to make development easier and more accessible.'}
+                                </p>
+                            </Link>
                         </div>
                     ))}
                 </div>
@@ -44,47 +46,50 @@ const Page: React.FC = () => {
                             <Link href={item.path} className="text-gray-900 dark:text-gray-50">
                                 {item.name}
                             </Link>
-                            <p className="text-sm text-gray-900 dark:text-gray-100 mt-2 leading-loose">
-                                {item.name === 'Accordion' && 'A collapsible section of content, useful for FAQs and more.'}
-                                {item.name === 'Alert' && 'Displays important messages in a prominent way to attract user attention.'}
-                                {item.name === 'AlertDialog' && 'A dialog that requires user acknowledgment before they can proceed.'}
-                                {item.name === 'Avatar' && 'Displays user profile images or icons.'}
-                                {item.name === 'Badge' && 'Small count and labeling component to signify status or counts.'}
-                                {item.name === 'Breadcrumbs' && 'Navigational aid to show the user’s current location in a hierarchy.'}
-                                {item.name === 'Button' && 'Triggers actions or events, such as submitting a form or saving data.'}
-                                {item.name === 'Card' && 'Container for grouping related information.'}
-                                {item.name === 'Carousel' && 'Displays a series of content in a slideshow format.'}
-                                {item.name === 'Checkbox' && 'Allows the user to select one or more options from a set.'}
-                                {item.name === 'Collapse' && 'Hides and shows content in a toggled manner.'}
-                                {item.name === 'Dialog' && 'A window overlaid on either the primary window or another dialog window.'}
-                                {item.name === 'Dropdown' && 'A list of options that appears when the user interacts with a button.'}
-                                {item.name === 'Error' && 'Displays error messages in a consistent manner.'}
-                                {item.name === 'Footer' && 'The bottom section of a webpage, often containing links and information.'}
-                                {item.name === 'Header' && 'The top section of a webpage, often containing navigation links and branding.'}
-                                {item.name === 'Input' && 'A field for user input, such as text, numbers, or passwords.'}
-                                {item.name === 'List' && 'Displays a list of items in an ordered or unordered format.'}
-                                {item.name === 'Modal' && 'A dialog box/popup window that is displayed on top of the current page.'}
-                                {item.name === 'Navbar' && 'A navigation bar for providing links to different sections of the site.'}
-                                {item.name === 'Notification' && 'Informs users about important updates or changes.'}
-                                {item.name === 'ProgressBar' && 'Visual representation of the progress of a task.'}
-                                {item.name === 'ProgressTracker' && 'Shows steps and progress through a multi-step process.'}
-                                {item.name === 'RadioButton' && 'Allows the user to select one option from a set.'}
-                                {item.name === 'Rating' && 'Allows users to provide ratings, typically with stars.'}
-                                {item.name === 'SearchBar' && 'Enables users to search through content.'}
-                                {item.name === 'Select' && 'A dropdown menu for selecting options.'}
-                                {item.name === 'SkeletonLoader' && 'Placeholder content while the actual content is loading.'}
-                                {item.name === 'Slider' && 'A control for selecting a value from a range.'}
-                                {item.name === 'Snackbar' && 'Brief messages that appear at the bottom of the screen.'}
-                                {item.name === 'Spinner' && 'Indicates loading or ongoing process.'}
-                                {item.name === 'StatusDot' && 'Indicates status with colored dots.'}
-                                {item.name === 'Stepper' && 'Indicates progress through a sequence of logical and numbered steps.'}
-                                {item.name === 'Snippet' && 'Displays code snippets with syntax highlighting.'}
-                                {item.name === 'Switch' && 'Toggle switches for binary options.'}
-                                {item.name === 'Tabs' && 'Navigational elements to switch between different views or sections.'}
-                                {item.name === 'TextArea' && 'A multi-line input field for longer text.'}
-                                {item.name === 'Toggle' && 'A switch that allows the user to toggle between two states.'}
-                                {item.name === 'Tooltip' && 'Displays additional information when hovering over an element.'}
-                            </p>
+                            <Link href={item.path}>
+                                <p className="text-sm text-gray-900 dark:text-gray-100 mt-2 leading-loose">
+                                    {item.name === 'Accordion' && 'A collapsible section of content, useful for FAQs and more.'}
+                                    {item.name === 'Alert' && 'Displays important messages in a prominent way to attract user attention.'}
+                                    {item.name === 'AlertDialog' && 'A dialog that requires user acknowledgment before they can proceed.'}
+                                    {item.name === 'Avatar' && 'Displays user profile images or icons.'}
+                                    {item.name === 'Badge' && 'Small count and labeling component to signify status or counts.'}
+                                    {item.name === 'Breadcrumbs' && 'Navigational aid to show the user’s current location in a hierarchy.'}
+                                    {item.name === 'Button' && 'Triggers actions or events, such as submitting a form or saving data.'}
+                                    {item.name === 'Card' && 'Container for grouping related information.'}
+                                    {item.name === 'Carousel' && 'Displays a series of content in a slideshow format.'}
+                                    {item.name === 'Checkbox' && 'Allows the user to select one or more options from a set.'}
+                                    {item.name === 'Collapse' && 'Hides and shows content in a toggled manner.'}
+                                    {item.name === 'Dialog' && 'A window overlaid on either the primary window or another dialog window.'}
+                                    {item.name === 'Dropdown' && 'A list of options that appears when the user interacts with a button.'}
+                                    {item.name === 'Error' && 'Displays error messages in a consistent manner.'}
+                                    {item.name === 'Footer' && 'The bottom section of a webpage, often containing links and information.'}
+                                    {item.name === 'Header' && 'The top section of a webpage, often containing navigation links and branding.'}
+                                    {item.name === 'Input' && 'A field for user input, such as text, numbers, or passwords.'}
+                                    {item.name === 'List' && 'Displays a list of items in an ordered or unordered format.'}
+                                    {item.name === 'Modal' && 'A dialog box/popup window that is displayed on top of the current page.'}
+                                    {item.name === 'Navbar' && 'A navigation bar for providing links to different sections of the site.'}
+                                    {item.name === 'Notification' && 'Informs users about important updates or changes.'}
+                                    {item.name === 'ProgressBar' && 'Visual representation of the progress of a task.'}
+                                    {item.name === 'ProgressTracker' && 'Shows steps and progress through a multi-step process.'}
+                                    {item.name === 'RadioButton' && 'Allows the user to select one option from a set.'}
+                                    {item.name === 'Rating' && 'Allows users to provide ratings, typically with stars.'}
+                                    {item.name === 'SearchBar' && 'Enables users to search through content.'}
+                                    {item.name === 'Select' && 'A dropdown menu for selecting options.'}
+                                    {item.name === 'SkeletonLoader' && 'Placeholder content while the actual content is loading.'}
+                                    {item.name === 'Slider' && 'A control for selecting a value from a range.'}
+                                    {item.name === 'Snackbar' && 'Brief messages that appear at the bottom of the screen.'}
+                                    {item.name === 'Spinner' && 'Indicates loading or ongoing process.'}
+                                    {item.name === 'StatusDot' && 'Indicates status with colored dots.'}
+                                    {item.name === 'Stepper' && 'Indicates progress through a sequence of logical and numbered steps.'}
+                                    {item.name === 'Snippet' && 'Displays code snippets with syntax highlighting.'}
+                                    {item.name === 'Switch' && 'Toggle switches for binary options.'}
+                                    {item.name === 'Tabs' && 'Navigational elements to switch between different views or sections.'}
+                                    {item.name === 'TextArea' && 'A multi-line input field for longer text.'}
+                                    {item.name === 'Toggle' && 'A switch that allows the user to toggle between two states.'}
+                                    {item.name === 'Tooltip' && 'Displays additional information when hovering over an element.'}
+                                </p>
+                            </Link>
+                            
                         </div>
                     ))}
                 </div>
