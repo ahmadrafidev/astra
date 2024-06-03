b4b83cc257010b97dec3bed13db453cbefad00f6
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-06-03 20:03:57 +0700

    fix gap in header nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,25 +8,24 @@ const Header = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 text-gray-900 dark:text-gray-50">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
             </svg>
-            <span className="sr-only">Astra UI</span>
             </Link>
-                <nav className="ml-auto flex gap-4 sm:gap-6">
-            <Link className="text-sm font-medium hover:underline underline-offset-4" href="/documentation">
-                <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-300 text-gray-900">
-                    Docs
-                </div>
-            </Link>
-            <Link className="text-sm font-medium hover:underline underline-offset-4" href="/documentation/components/accordion">
-                <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-300 text-gray-900">
-                    Components
-                </div>
-            </Link>
-            <Link className="text-sm font-medium hover:underline underline-offset-4" href="/contact">
-                <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-300 text-gray-900">
-                    Connect!
-                </div>
-            </Link>
-            <ThemeToggle />
+            <nav className="ml-auto flex gap-2 sm:gap-4">
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/documentation">
+                    <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-300 text-gray-900">
+                        Docs
+                    </div>
+                </Link>
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/documentation/components/accordion">
+                    <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-300 text-gray-900">
+                        Components
+                    </div>
+                </Link>
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/contact">
+                    <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm dark:bg-gray-300 text-gray-900">
+                        Connect!
+                    </div>
+                </Link>
+                <ThemeToggle />
             </nav>
         </header>
     );
