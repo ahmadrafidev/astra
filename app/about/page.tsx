70c0f2ac019bcb6b7708fda9d1fa07ab84b89e0c
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-07-03 16:44:01 +0700

    remove unnecessary icon

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ import { foundationList, componentsList } from '../../utils/constants/route';
 const AboutAstraPage: React.FC = () => {
     return (
         <Layout>
-            <h1 className="text-2xl md:text-4xl font-medium mb-4 text-gray-900 dark:text-white">Astra UI Overview</h1>
+            <h1 className="text-2xl md:text-4xl font-medium mb-4 text-gray-900 dark:text-white">Overview</h1>
             <div className="mb-8">
                 <h2 className="text-xl md:text-2xl font-medium mb-4 text-gray-900 dark:text-gray-50">Purposes</h2>
                 <p className="text-sm md:text-base mb-4 leading-relaxed text-gray-900 dark:text-gray-50">
@@ -33,10 +33,8 @@ const AboutAstraPage: React.FC = () => {
                 </ul>
                 <div className="flex flex-col ">
                     <p className="text-sm md:text-base mb-4 leading-relaxed text-gray-900 dark:text-gray-50">
-                        Astra UI aims to be a versatile toolset for developers, enabling them to create beautiful, functional, and accessible web applications with ease. Whether you are building a simple website or a complex web application, Astra UI provides the foundational components you need to get started quickly and efficiently.
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 inline-block ml-2">
-                            <path d="M15.98 1.804a1 1 0 0 0-1.96 0l-.24 1.192a1 1 0 0 1-.784.785l-1.192.238a1 1 0 0 0 0 1.962l1.192.238a1 1 0 0 1 .785.785l.238 1.192a1 1 0 0 0 1.962 0l.238-1.192a1 1 0 0 1 .785-.785l1.192-.238a1 1 0 0 0 0-1.962l-1.192-.238a1 1 0 0 1-.785-.785l-.238-1.192ZM6.949 5.684a1 1 0 0 0-1.898 0l-.683 2.051a1 1 0 0 1-.633.633l-2.051.683a1 1 0 0 0 0 1.898l2.051.684a1 1 0 0 1 .633.632l.683 2.051a1 1 0 0 0 1.898 0l.683-2.051a1 1 0 0 1 .633-.633l2.051-.683a1 1 0 0 0 0-1.898l-2.051-.683a1 1 0 0 1-.633-.633L6.95 5.684ZM13.949 13.684a1 1 0 0 0-1.898 0l-.184.551a1 1 0 0 1-.632.633l-.551.183a1 1 0 0 0 0 1.898l.551.183a1 1 0 0 1 .633.633l.183.551a1 1 0 0 0 1.898 0l.184-.551a1 1 0 0 1 .632-.633l.551-.183a1 1 0 0 0 0-1.898l-.551-.184a1 1 0 0 1-.633-.632l-.183-.551Z" />
-                        </svg>
+                        Astra UI aims to be a versatile toolset for developers, enabling them to create beautiful, functional, and accessible web applications with ease. 
+                        Whether you are building a simple website or a complex web application, Astra UI provides the foundational components you need to get started quickly and efficiently.
                     </p>
                 </div>
             </div>
