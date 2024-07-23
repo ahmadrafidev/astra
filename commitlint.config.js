590d5aefd33a8a3f95267e26adfdd9474b9c272b
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-07-23 16:36:24 +0700

    adding new packages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const conventional = require("@commitlint/config-conventional");
+
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    plugins: ["commitlint-plugin-function-rules"],
+    rules: {
+    ...conventional.rules,
+    "type-enum": [
+        2,
+        "always",
+        ["feat", "feature", "fix", "refactor", "docs", "build", "test", "ci", "chore"],
+    ],
+    "function-rules/header-max-length": [0],
+    },
+};
