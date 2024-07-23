a1ddc1334fa8a8c015302e861d4e47bbff5c6f6e
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-07-23 17:54:05 +0700

    feat: adding initial content for data viz

--- app/foundations/data-visualization/page.tsx
+++ app/foundations/data-visualization/page.tsx
@@ -16,7 +16,69 @@ const AstraDataVisualizationPage: React.FC = () => {
     return (
         <Layout>
             <Badge text="Foundations" />
-            <h1 className="text-2xl md:text-4xl font-medium mb-4 text-gray-900 dark:text-gray-50">Data visualization</h1>
+            <h1 className="text-2xl md:text-4xl font-medium mb-4 text-gray-900 dark:text-gray-50">Data Visualization</h1>
+            <main>
+                <section className="mb-3 md:mb-4">
+                    <h2 className="text-2xl md:text-3xl font-medium mb-2 md:mb-3 text-gray-900 dark:text-gray-50">Overview</h2>
+                    <p className="mb-4 text-gray-700 dark:text-gray-300 text-justify">
+                        Data visualization involves the graphical representation of data to improve understanding and decision-making. 
+                        Effective data visualizations are accurate, purposeful, and responsive, ensuring they convey information clearly across different devices and contexts. 
+                        They are used to monitor, analyze, explore, and report data, making complex information more accessible and actionable.
+                    </p>
+                </section>
+                <section className="mb-3 md:mb-4"> 
+                    <h2 className="text-2xl md:text-3xl mb-2 md:mb-3 font-medium text-gray-900 dark:text-gray-50">Principles</h2>
+                    <ul className="text-gray-900 dark:text-gray-50 space-y-3 md:space-y-4">
+                        <li>
+                            <h3 className="text-lg font-medium">Clarity and Simplicity</h3>
+                            <p className="text-base font-sans font-normal">Ensure visualizations are easy to understand and free from unnecessary complexity.</p>
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">Consistency</h3>
+                            <p className="text-base font-sans font-normal">Maintain uniformity in design elements such as colors, fonts, and layouts.</p>
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">Accessibility</h3>
+                            <p className="text-base font-sans font-normal">Design with accessibility in mind to ensure all users can understand the visualizations.</p>
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">Relevance</h3>
+                            <p className="text-base font-sans font-normal">Ensure that visualizations are relevant and convey the necessary information effectively.</p>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h2 className="text-2xl md:text-3xl font-medium mb-4 text-gray-900 dark:text-gray-50">Use Cases</h2>
+                    <ul className="text-gray-900 dark:text-gray-50 space-y-3 md:space-y-4">
+                        <li>
+                            <h3 className="text-lg font-medium">
+                                Monitoring
+                            </h3>
+                            
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">
+                                Analysis
+                            </h3>   
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">
+                                Exploration
+                            </h3>  
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">
+                                Optimization
+                            </h3> 
+                        </li>
+                        <li>
+                            <h3 className="text-lg font-medium">
+                                Reporting
+                            </h3>
+                        </li>
+                    </ul>
+                </section>
+            </main>
         </Layout>
     );
 };
