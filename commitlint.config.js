1c6dccd85d5bdc513164ebd4ebdd3b5c3a6b76e5
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-07-23 17:09:40 +0700

    chore: change to pnpm workspaces

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,1 @@
-const conventional = require("@commitlint/config-conventional");
-
-module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    plugins: ["commitlint-plugin-function-rules"],
-    rules: {
-    ...conventional.rules,
-    "type-enum": [
-        2,
-        "always",
-        ["feat", "feature", "fix", "refactor", "docs", "build", "test", "ci", "chore"],
-    ],
-    "function-rules/header-max-length": [0],
-    },
-};
+module.exports = { extends: ['@commitlint/config-conventional'] };
