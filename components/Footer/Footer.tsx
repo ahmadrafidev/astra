1d62b61025f211f7518b32f5a73002d35fc99606
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-07-29 10:42:08 +0700

    chore: adding footer aria label

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,11 +6,11 @@ import Link from 'next/link';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="flex flex-col sm:flex-row py-6 w-full items-center px-4 md:px-12 bg-gray-50 dark:bg-black justify-between">
+        <footer className="flex flex-col sm:flex-row py-6 w-full items-center px-4 md:px-12 bg-gray-50 dark:bg-black justify-between" aria-label="Main Footer">
             <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300">
-                Engineered with passion by <Link href="https://x.com/rafiwiranaa" passHref className="underline">Rafi</Link>.
+                Engineered with passion by <Link href="https://x.com/rafiwiranaa" passHref className="underline" aria-label="Visit Rafi's Twitter">Rafi</Link>.
             </p>
-            <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300">
+            <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300" aria-label="Copyright Information">
                 © 2024 AstraUI
             </p>
         </footer>
