9d366590dd29fe8e6bbf87b7a71b56a8171144ec
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-03 16:07:52 +0700

    fix error component storybook

--- stories/Error.stories.tsx
+++ stories/Error.stories.tsx
@@ -11,11 +11,26 @@ const Template: StoryFn<ErrorProps> = (args) => <Error {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
+    type: 'network',
+    title: 'Error',
     message: 'Something went wrong. Please try again.',
+    primaryAction: {
+        label: 'Retry',
+        onClick: () => alert('Retrying...'),
+    },
 };
 
-export const WithRetry = Template.bind({});
-WithRetry.args = {
-    message: 'Something went wrong. Please try again.',
-    onRetry: () => alert('Retrying...'),
+export const WithSecondaryAction = Template.bind({});
+WithSecondaryAction.args = {
+    type: 'upload',
+    title: 'Upload Error',
+    message: 'The file could not be uploaded. Please try again.',
+    primaryAction: {
+        label: 'Retry',
+        onClick: () => alert('Retrying'),
+    },
+    secondaryAction: {
+        label: 'Cancel',
+        onClick: () => alert('Cancelling'),
+    },
 };
