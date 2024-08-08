4b52c1a058f895d720b6b79996177beb567d35c3
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-08 14:25:39 +0700

    feat: add PIN Code component

--- components/PinCode/PinCode.tsx
+++ components/PinCode/PinCode.tsx
@@ -1,17 +1,100 @@
-// components/PhoneNumber/PhoneNumber.tsx
-import React from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 
+/**
+ * Props for the PinCode component.
+ * 
+ * @typedef {Object} PinCodeProps
+ * @property {number} [length=6] - The length of the PIN code.
+ * @property {string} [className] - Additional class names for custom styling.
+ * @property {string} [inputClassName] - Additional class names for the input elements.
+ * @property {boolean} [isMask=true] - Whether to mask the input values.
+ * @property {(pin: string) => void} [onComplete] - Callback when the PIN code input is complete.
+ * @property {string[]} [pin=[]] - The current PIN code.
+ * @property {(pin: string[]) => void} [onPinChange] - Callback when the PIN code changes.
+ */
 export interface PinCodeProps {
-    className?: string;
+  length?: number;
+  className?: string;
+  inputClassName?: string;
+  isMask?: boolean;
+  pin?: string[];
+  onComplete?: (pin: string) => void;
+  onPinChange?: (pin: string[]) => void;
 }
 
-const PinCode: React.FC<PinCodeProps> = ({ className }) => {
-    return (
-        <div className={`flex text-gray-700 dark:text-gray-200 ${className}`} aria-label="PinCode">
-            <p>PinCode</p>
-        </div>
-    );
+/**
+ * PinCode component for inputting a PIN code.
+ * 
+ * @component
+ * @param {PinCodeProps} props - Props for the PinCode component.
+ * @returns {JSX.Element} The rendered PinCode component.
+ */
+const PinCode: React.FC<PinCodeProps> = ({
+  length = 6,
+  className,
+  inputClassName,
+  isMask = true,
+  pin = [],
+  onPinChange,
+  onComplete,
+}) => {
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (pin.length === 0) {
+      onPinChange?.(new Array(length).fill(''));
+    }
+  }, [length, pin.length, onPinChange]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    // there is a bug here
+    const { value } = e.target;
+    if (/^[0-9]?$/.test(value)) {
+      setError(null);
+      const newPin = [...pin];
+      newPin[index] = value;
+      onPinChange?.(newPin);
+
+      if (value && index < length - 1) {
+        const nextInput = document.getElementById(`pin-${index + 1}`);
+        nextInput?.focus();
+      }
+
+      if (value && index === length - 1 && onComplete) {
+        onComplete(newPin.join(''));
+      }
+    } else {
+      setError('Only numeric values are allowed');
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    if (e.key === 'Backspace' && !pin[index] && index > 0) {
+      const prevInput = document.getElementById(`pin-${index - 1}`);
+      prevInput?.focus();
+    }
+  };
+
+  return (
+    <div className={`flex flex-col space-y-1 ${className}`}>
+      <div className="flex space-x-2" role="group" aria-label="PIN Code">
+        {pin.map((value, index) => (
+          <input
+            key={index}
+            id={`pin-${index}`}
+            type={isMask ? 'password' : 'text'}
+            maxLength={1}
+            value={value}
+            onChange={(e) => handleChange(e, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+            className={`w-12 h-12 text-center text-lg bg-gray-300 dark:bg-gray-800 text-gray-800 dark:text-gray-50 border border-gray-400 dark:border-gray-600 rounded ${inputClassName}`}
+            aria-label={`Digit ${index + 1}`}
+          />
+        ))}
+      </div>
+      {error && <p className="text-red-600 mt-2">{error}</p>}
+    </div>
+  );
 };
 
 PinCode.displayName = "PinCode";
--- components/PinCode/PinCodeClient.tsx
+++ components/PinCode/PinCodeClient.tsx
@@ -5,7 +5,27 @@ import React, { useState } from 'react';
 import Layout from '../Layout/Layout';
 import Badge from '../Badge/Badge';
 
+import PinCode from './PinCode';
+
 const PinCodeClient: React.FC = () => {
+    const [defaultPin, setDefaultPin] = useState<string[]>(new Array(6).fill(''));
+    const [maskedPin, setMaskedPin] = useState<string[]>(new Array(6).fill(''));
+
+    const handleDefaultPinComplete = (pin: string) => {
+        console.log('Entered Default PIN:', pin);
+    };
+
+    const handleMaskedPinComplete = (pin: string) => {
+        console.log('Entered Masked PIN:', pin);
+    };
+
+    const handleDefaultPinChange = (newPin: string[]) => {
+        setDefaultPin(newPin);
+    };
+
+    const handleMaskedPinChange = (newPin: string[]) => {
+        setMaskedPin(newPin);
+    };
 
     return (
         <Layout>
@@ -16,30 +36,101 @@ const PinCodeClient: React.FC = () => {
                     A secure input for PIN codes or short numeric sequences.
                 </p>
             </div>
+            <section className="my-5 space-y-4">
+                <h2 className="text-xl md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Variants</h2>
+                <div>
+                    <h3 className="text-lg md:text-xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Default PIN Code</h3>
+                    <PinCode
+                        className="flex justify-center" 
+                        inputClassName="custom-input-class" 
+                        isMask={false} 
+                        onComplete={handleDefaultPinComplete} 
+                        pin={defaultPin}
+                        onPinChange={handleDefaultPinChange}
+                    />
+                </div>
+                <div>
+                    <h3 className="text-lg md:text-xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Masked PIN Code</h3>
+                    <PinCode
+                        className="flex justify-center" 
+                        inputClassName="custom-input-class" 
+                        isMask={true} 
+                        onComplete={handleMaskedPinComplete} 
+                        pin={maskedPin}
+                        onPinChange={handleMaskedPinChange}
+                    />
+                </div>
+            </section>
             {/* Props Section */}
             <section className="mb-10">
-                <h2 className="text-xl md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Props</h2>
-                <div className="bg-gray-50 dark:bg-gray-800 p-4 rounded-lg shadow">
-                    <table className="min-w-full table-auto">
-                        <thead>
-                            <tr>
-                                <th className="px-6 py-2 text-left text-gray-700 dark:text-gray-200">Name</th>
-                                <th className="px-6 py-2 text-left text-gray-700 dark:text-gray-200 font-mono">Type</th>
-                                <th className="px-6 py-2 text-left text-gray-700 dark:text-gray-200">Default</th>
-                                <th className="px-6 py-2 text-left text-gray-700 dark:text-gray-200">Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
+                <h2 className="text-xl md:text-2xl font-semibold text-gray-900 dark:text-gray-50 mb-6">Props</h2>
+                <div className="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <thead className="bg-gray-50 dark:bg-gray-700">
+                    <tr>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Name</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Type</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Default</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Description</th>
+                    </tr>
+                    </thead>
+                    <tbody className="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">length</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">number</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">6</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Number of digits in the PIN code.</td>
+                    </tr>
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">className</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Custom class for the container.</td>
+                    </tr>
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">inputClassName</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Custom class for each input box.</td>
+                    </tr>
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">isMask</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">boolean</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">true</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Whether the input should be masked.</td>
+                    </tr>
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">onComplete</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`(pin: string) => void`}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Callback function when the PIN is completely entered.</td>
+                    </tr>
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">pin</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string[]</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">[]</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The current value of the PIN code as an array of strings.</td>
+                    </tr>
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">onPinChange</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`(pin: string[]) => void`}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
+                        <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Callback function when the PIN code changes.</td>
+                    </tr>
+                    </tbody>
+                </table>
                 </div>
             </section>
             {/* Best Practices Section */}
             <section>
-                <h2 className="text-2xl md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Best Practices</h2>
+                <h2 className="text-xl md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Best Practices</h2>
                 <div className="space-y-4">
                     <ul className="list-decimal list-inside text-gray-700 dark:text-gray-300 space-y-3">
-                        <li>Ensure the input field has appropriate aria attributes for accessibility.</li>
+                        <li>Provide clear feedback to the user when the PIN is on progress and complete.</li>
+                        <li>Ensure the input fields are clearly visible and accessible on both light and dark themes.</li>
+                        <li>Validate the PIN input to ensure it meets the expected format and length.</li>
+                        <li>Handle focus and blur events to enhance the user experience, especially on mobile devices.</li>
+                        <li>Use masking for security, especially for sensitive PIN codes.</li>
                     </ul>
                 </div>
             </section>
