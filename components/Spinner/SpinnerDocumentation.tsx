7bdb3c12ceed72facc21cbbafee60c107d0c613c
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-17 15:16:39 +0700

    feat: improve spinner client side

--- components/Spinner/SpinnerDocumentation.tsx
+++ components/Spinner/SpinnerDocumentation.tsx
@@ -4,8 +4,8 @@ const SpinnerDocumentation = () => {
   return (
     <>
       {/* Props Section */}
-      <section className="my-5">
-        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-6">Props</h2>
+      <section id="properties" className="my-5 scroll-mt-20">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-5">Props</h2>
         <div className="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
           <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
             <thead className="bg-gray-50 dark:bg-gray-700">
@@ -19,14 +19,14 @@ const SpinnerDocumentation = () => {
             <tbody className="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">size</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`'sm' | 'md' | 'lg' | number`}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono text-wrap">{`'sm' | 'md' | 'lg' | number`}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">{`'md'`}</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The size of the spinner. Can be a predefined size or a custom number in pixels.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">color</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The color of the spinner. Can be any valid Tailwind CSS color class.</td>
               </tr>
               <tr>
@@ -37,14 +37,14 @@ const SpinnerDocumentation = () => {
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">speed</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`'slow' | 'normal' | 'fast'`}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono text-wrap">{`'slow' | 'normal' | 'fast'`}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">{`'normal'`}</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The animation speed of the spinner.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">className</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional classes for styling the spinner.</td>
               </tr>
               <tr>
@@ -57,9 +57,10 @@ const SpinnerDocumentation = () => {
           </table>
         </div>
       </section>
+
       {/* Best Practices Section */}
-      <section>
-        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-6">Best Practices</h2>
+      <section id="best-practices" className="scroll-mt-20">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-5">Best Practices</h2>
         <div className="space-y-4">
           <ul className="list-decimal list-inside text-gray-700 dark:text-gray-300 space-y-3">
             <li>Use spinners to indicate loading or ongoing processes.</li>
--- components/Spinner/SpinnerPageClient.tsx
+++ components/Spinner/SpinnerPageClient.tsx
@@ -1,6 +1,12 @@
-'use client'
+'use client';
 
 import React, { useState } from 'react';
+
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+import TOC from '@/components/TOC/TOC';
+import { Tabs, Tab } from '@/components/Tabs/Tabs';
 
 import Layout from '../Layout/Layout';
 import Badge from '../Badge/Badge';
@@ -18,62 +24,122 @@ const SpinnerPageClient: React.FC = () => {
         }
     };
 
+    const tocItems = [
+        { id: 'overview', label: 'Overview' },
+        { id: 'usage', label: 'Usage' },
+        { id: 'properties', label: 'Properties' },
+        { id: 'best-practices', label: 'Best Practices' },
+    ];
+
+    const codeExample = `
+    <Spinner size="sm" />
+    <Spinner size="md" />
+    <Spinner size="lg" />
+    <Spinner color="border-blue-500" />
+    <Spinner thickness={4} speed="slow" />
+    <Spinner className="shadow-lg border-red-500 shadow-red-500" />
+    `;
+
     return (
         <Layout>
-            <Badge text="Components" />
-            <div className="flex flex-col mb-5">
-                <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Spinner</h1>
-                <p className="text-sm md:text-base font-normal font-sans text-gray-900 dark:text-gray-50">
-                    An animated indicator showing that content is loading or processing. Highly customizable and accessible.
-                </p>
+            <div className="flex">
+                <div className="w-4/5 pr-2">
+                    <Badge text="Components" />
+                    <div id="overview" className="flex flex-col mb-5 scroll-mt-16">
+                        <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Spinner</h1>
+                        <p className="text-sm md:text-base font-normal font-sans text-gray-900 dark:text-gray-50">
+                            An animated indicator showing that content is loading or processing. Highly customizable and accessible.
+                        </p>
+                    </div>
+
+                    {/* Variants Section */}
+                    <section id="usage" className="scroll-mt-20">
+                        <Tabs>
+                            <Tab label="Preview">
+                                <div className="relative w-full overflow-hidden mt-4 rounded-lg bg-zinc-100 dark:bg-zinc-950 shadow border border-slate-300 dark:border-slate-800">
+                                    <div className="p-6 space-y-4 flex flex-col justify-center items-center">
+                                        <div className="w-full flex flex-col items-center">
+                                            <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Basic Usage</h2>
+                                            <div className="flex space-x-4">
+                                                <Spinner size="sm" />
+                                                <Spinner size="md" />
+                                                <Spinner size="lg" />
+                                            </div>
+                                        </div>
+                                        <div className="w-full flex flex-col items-center">
+                                            <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Custom Colors</h2>
+                                            <div className="flex space-x-4">
+                                                <Spinner color="border-blue-500" />
+                                                <Spinner color="border-green-500" />
+                                                <Spinner color="border-red-500" />
+                                                <Spinner color="border-purple-500" />
+                                            </div>
+                                        </div>
+                                        <div className="w-full flex flex-col items-center">
+                                            <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Custom Thickness and Speed</h2>
+                                            <div className="flex space-x-4">
+                                                <Spinner thickness={2} speed="slow" />
+                                                <Spinner thickness={4} speed="normal" />
+                                                <Spinner thickness={6} speed="fast" />
+                                            </div>
+                                        </div>
+                                        <div className="w-full flex flex-col items-center">
+                                            <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Spinner in a Button</h2>
+                                            <button 
+                                                className="bg-blue-500 text-white px-4 py-2 rounded flex items-center space-x-2"
+                                                onClick={handleLoadingToggle}
+                                                disabled={isLoading}
+                                            >
+                                                {isLoading && <Spinner className="z-50" size="sm" color="text-white" />}
+                                                <span className="font-medium font-sans">{isLoading ? 'Loading' : 'Click to load'}</span>
+                                            </button>
+                                        </div>
+                                        <div className="w-full flex flex-col items-center">
+                                            <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">With Custom Styling</h2>
+                                            <Spinner className="shadow-lg border-red-500 shadow-red-500" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </Tab>
+                            <Tab label="Code">
+                                <div
+                                    className="relative rounded-lg bg-gray-900 dark:bg-gray-800/90 mt-4"
+                                    role="region"
+                                    aria-label="Code Snippet in tsx"
+                                >
+                                    <div className="flex justify-between items-center px-4 py-2 border-b border-gray-800">
+                                        <span className="text-sm font-medium text-gray-100">
+                                            tsx
+                                        </span>
+                                    </div>
+                                    <div className="overflow-auto rounded-lg border border-gray-800">
+                                        <SyntaxHighlighter
+                                            language="tsx"
+                                            style={materialLight}
+                                            showLineNumbers={false}
+                                            wrapLongLines={true}
+                                            customStyle={{
+                                                margin: 1.5,
+                                                borderRadius: '0.5rem 0.5rem 0.5rem 0.5rem',
+                                                fontSize: '14px',
+                                                lineHeight: '1.25',
+                                                padding: '10px',
+                                            }}
+                                        >
+                                            {codeExample}
+                                        </SyntaxHighlighter>
+                                    </div>
+                                </div>
+                            </Tab>
+                        </Tabs>
+                    </section>
+                    <SpinnerDocumentation />
+                </div>
+                <TOC items={tocItems} />
             </div>
-            <section className="space-y-5">
-                <div>
-                    <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Basic Usage</h2>
-                    <div className="flex space-x-4">
-                        <Spinner size="sm" />
-                        <Spinner size="md" />
-                        <Spinner size="lg" />
-                    </div>
-                </div>
-                <div>
-                    <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Custom Colors</h2>
-                    <div className="flex space-x-4">
-                        <Spinner color="border-blue-500" />
-                        <Spinner color="border-green-500" />
-                        <Spinner color="border-red-500" />
-                        <Spinner color="border-purple-500" />
-                    </div>
-                </div>
-                <div>
-                    <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Custom Thickness and Speed</h2>
-                    <div className="flex space-x-4">
-                        <Spinner thickness={2} speed="slow" />
-                        <Spinner thickness={4} speed="normal" />
-                        <Spinner thickness={6} speed="fast" />
-                    </div>
-                </div>
-                <div>
-                    <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Spinner in a Button</h2>
-                    <button 
-                        className="bg-blue-500 text-white px-4 py-2 rounded flex items-center space-x-2"
-                        onClick={handleLoadingToggle}
-                        disabled={isLoading}
-                    >
-                        {isLoading && <Spinner className="z-50" size="sm" color="text-white" />}
-                        <span className="font-medium font-sans">{isLoading ? 'Loading' : 'Click to load'}</span>
-                    </button>
-                </div>
-                <div>
-                    <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-50">With Custom Styling</h2>
-                    <Spinner className="shadow-lg border-red-500 shadow-red-500" />
-                </div>
-            </section>
-            <SpinnerDocumentation />
         </Layout>
     );
 };
 
 SpinnerPageClient.displayName = "SpinnerPageClient";
-
 export default SpinnerPageClient;
