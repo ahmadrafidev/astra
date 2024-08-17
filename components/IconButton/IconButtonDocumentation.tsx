bd0a3e4ed6fa0d19d2a359f0cd4729dd68c7d2ff
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-17 13:31:58 +0700

    feat: improving icon button client page

--- components/IconButton/IconButtonDocumentation.tsx
+++ components/IconButton/IconButtonDocumentation.tsx
@@ -4,8 +4,8 @@ const IconButtonDocumentation = () => {
   return (
     <>
       {/* Props Table Section */}
-      <section className="my-5">
-        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-6">Props</h2>
+      <section id="properties" className="my-5 scroll-mt-20">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-5">Props</h2>
         <div className="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
           <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
             <thead className="bg-gray-50 dark:bg-gray-700">
@@ -20,36 +20,36 @@ const IconButtonDocumentation = () => {
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">icon</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">ElementType</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">none</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The icon component to display inside the button.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">onClick</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">function</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">none</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The function to call when the button is clicked.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">aria-label</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">none</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The ARIA label for the button, used to improve accessibility.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">className</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for custom styling.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">iconClassName</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for styling the icon.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">variant</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`"contained" | "outlinedBorder" | "outlinedNoBorder"`}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono text-wrap">{`"contained" | "outlinedBorder" | "outlinedNoBorder"`}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">{`"contained"`}</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The variant of the button, determining its style.</td>
               </tr>
@@ -62,16 +62,17 @@ const IconButtonDocumentation = () => {
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">color</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">none</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Custom color for the button.</td>
               </tr>
             </tbody>
           </table>
         </div>
       </section>
+
       {/* Best Practices Section */}
-      <section className="my-5">
-        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-6">Best Practices</h2>
+      <section id="best-practices" className="my-5 scroll-mt-20">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-5">Best Practices</h2>
         <div className="space-y-4">
           <ul className="list-decimal list-inside text-gray-700 dark:text-gray-300 space-y-3">
             <li>Use clear and intuitive icons that represent the action they perform.</li>
--- components/IconButton/IconButtonPageClient.tsx
+++ components/IconButton/IconButtonPageClient.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import React from 'react';
+
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import * as RadixIcons from '@radix-ui/react-icons';
+
+import TOC from '@/components/TOC/TOC';
+import { Tabs, Tab } from '@/components/Tabs/Tabs';
 
 import Layout from '../Layout/Layout';
 import Badge from '../Badge/Badge';
@@ -15,65 +21,100 @@ const icons = [
   { icon: RadixIcons.PlusIcon, label: 'Add', ariaLabel: 'Add' },
 ];
 
+const tocItems = [
+  { id: 'overview', label: 'Overview' },
+  { id: 'usage', label: 'Usage' },
+  { id: 'properties', label: 'Properties' },
+  { id: 'best-practices', label: 'Best Practices' },
+];
+
+const codeExample = `
+  <IconButton 
+    icon={RadixIcons.TrashIcon} 
+    onClick={() => alert('Delete button clicked')} 
+    aria-label="Delete" 
+    variant="contained" 
+    color="gray" 
+  />
+`;
+
 const IconButtonPageClient: React.FC = () => {
   return (
     <Layout>
-      <Badge text="Components" />
-      <div className="flex flex-col mb-5">
-        <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Icon Button</h1>
-        <p className="text-sm md:text-base font-normal font-sans text-gray-900 dark:text-gray-50">
-          A clickable button represented by an icon without accompanying text.
-        </p>
+      <div className="flex">
+        <div className="w-4/5 pr-2">
+          <Badge text="Components" />
+          <div id="overview" className="flex flex-col mb-5 scroll-mt-16">
+            <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Icon Button</h1>
+            <p className="text-sm md:text-base font-normal font-sans text-gray-900 dark:text-gray-50">
+              A clickable button represented by an icon without accompanying text.
+            </p>
+          </div>
+
+          {/* Usage Section */}
+          <section id="usage" className="scroll-mt-20">
+            <Tabs>
+              <Tab label="Preview">
+                <div className="relative w-full overflow-hidden mt-4 rounded-lg flex justify-center items-center bg-zinc-100 dark:bg-zinc-950 shadow border border-slate-300 dark:border-slate-800">
+                  <div className="w-3/4 space-y-6">
+                    <div className="space-y-4 flex flex-col justify-center items-center p-6">
+                      {['contained', 'outlinedBorder', 'outlinedNoBorder'].map((variant) => (
+                        <div key={variant} className="flex flex-col justify-center items-center">
+                          <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3 capitalize">
+                            {variant.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase())} Variant
+                          </h3>
+                          <div className="flex space-x-4">
+                            {icons.map((icon, index) => (
+                              <IconButton
+                                key={index}
+                                icon={icon.icon}
+                                onClick={() => alert(`${icon.label} button clicked`)}
+                                aria-label={icon.ariaLabel}
+                                variant={variant as 'contained' | 'outlinedBorder' | 'outlinedNoBorder'}
+                                color="gray"
+                              />
+                            ))}
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </Tab>
+              <Tab label="Code">
+                <div
+                  className="relative rounded-lg bg-gray-900 dark:bg-gray-800/90 mt-4"
+                  role="region"
+                  aria-label="Code Snippet in tsx"
+                >
+                  <div className="flex justify-between items-center px-4 py-2 border-b border-gray-800">
+                    <span className="text-sm font-medium text-gray-100">tsx</span>
+                  </div>
+                  <div className="overflow-auto rounded-lg border border-gray-800">
+                    <SyntaxHighlighter
+                      language="tsx"
+                      style={materialLight}
+                      showLineNumbers={false}
+                      wrapLongLines={true}
+                      customStyle={{
+                        margin: 1.5,
+                        borderRadius: '0.5rem 0.5rem 0.5rem 0.5rem',
+                        fontSize: '14px',
+                        lineHeight: '1.25',
+                        padding: '10px',
+                      }}
+                    >
+                      {codeExample}
+                    </SyntaxHighlighter>
+                  </div>
+                </div>
+              </Tab>
+            </Tabs>
+          </section>
+          <IconButtonDocumentation />
+        </div>
+        <TOC items={tocItems} />
       </div>
-      <section className="mb-5">
-        <h2 className="text-lg md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Variants</h2>
-        <div className="space-y-4 flex flex-col justify-around">
-          {['contained', 'outlinedBorder', 'outlinedNoBorder'].map(variant => (
-            <div key={variant}>
-              <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3 capitalize">
-                {variant.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())} Variant
-              </h3>
-              <div className="flex space-x-4">
-                {icons.map((icon, index) => (
-                  <IconButton
-                    key={index}
-                    icon={icon.icon}
-                    onClick={() => alert(`${icon.label} button clicked`)}
-                    aria-label={icon.ariaLabel}
-                    variant={variant as 'contained' | 'outlinedBorder' | 'outlinedNoBorder'}
-                    color="gray"
-                  />
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <section className="mb-5">
-        <h2 className="text-lg md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Sizes</h2>
-        <div className="space-y-4 flex flex-col justify-around">
-          {['sm', 'md', 'lg'].map(size => (
-            <div key={size}>
-              <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3 capitalize">
-                {size === 'sm' ? 'Small' : size === 'md' ? 'Medium' : 'Large'} Size
-              </h3>
-              <div className="flex space-x-4">
-                {icons.map((icon, index) => (
-                  <IconButton
-                    key={index}
-                    icon={icon.icon}
-                    onClick={() => alert(`${icon.label} button clicked`)}
-                    aria-label={icon.ariaLabel}
-                    size={size as 'sm' | 'md' | 'lg'}
-                    color="gray"
-                  />
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <IconButtonDocumentation />
     </Layout>
   );
 };
