a23914383b57327108defc4e48b89ddd1cdb55c5
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-08-20 01:00:22 +0700

    feat: improve input client side

--- components/Input/InputDocumentation.tsx
+++ components/Input/InputDocumentation.tsx
@@ -4,8 +4,8 @@ const InputDocumentation = () => {
   return (
     <>
       {/* Props Table Section */}
-      <section className="my-5">
-        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-6">Props</h2>
+      <section id="properties" className="my-5 scroll-mt-20">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-5">Props</h2>
         <div className="bg-white dark:bg-gray-800 rounded-lg shadow overflow-hidden">
           <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
             <thead className="bg-gray-50 dark:bg-gray-700">
@@ -19,80 +19,80 @@ const InputDocumentation = () => {
             <tbody className="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">type</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`'text' | 'password' | 'email'`}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono text-wrap">{`'text' | 'password' | 'email'`}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">{`'text'`}</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The type of input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">value</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">N/A</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The value of the input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">onChange</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">{`(e: React.ChangeEvent<HTMLInputElement>) => void`}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">N/A</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono text-wrap">{`React.ChangeEvent`}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The function to call when the input value changes.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">placeholder</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The placeholder text for the input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">label</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">The label for the input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">hint</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">A hint for the input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">error</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">An error message for the input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">icon</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">React.ReactNode</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">N/A</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">An optional icon to display inside the input.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">className</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for the wrapper div.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">inputClassName</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for the input element.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">labelClassName</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for the label.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">hintClassName</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for the hint.</td>
               </tr>
               <tr>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">errorClassName</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300 font-mono">string</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">empty</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">-</td>
                 <td className="px-6 py-4 text-sm text-gray-500 dark:text-gray-300">Additional class names for the error message.</td>
               </tr>
               <tr>
@@ -105,9 +105,10 @@ const InputDocumentation = () => {
           </table>
         </div>
       </section>
+
       {/* Best Practices Section */}
-      <section>
-        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-6">Best Practices</h2>
+      <section id="best-practices" className="scroll-mt-20">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-50 mb-5">Best Practices</h2>
         <div className="space-y-4">
           <ul className="list-decimal list-inside text-gray-700 dark:text-gray-300 space-y-3">
             <li>Use clear and intuitive icons that represent the action they perform.</li>
--- components/Input/InputPageClient.tsx
+++ components/Input/InputPageClient.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import React, { useState } from 'react';
-import { InformationCircleIcon, LockClosedIcon, UserIcon } from '@heroicons/react/24/outline';
+
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { InformationCircleIcon, LockClosedIcon } from '@heroicons/react/24/outline';
+
+import TOC from '@/components/TOC/TOC';
+import { Tabs, Tab } from '@/components/Tabs/Tabs';
 
 import Layout from '../Layout/Layout';
 import Badge from '../Badge/Badge';
@@ -10,121 +16,178 @@ import Input from './Input';
 import InputDocumentation from './InputDocumentation';
 
 const InputPageClient: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const [search, setSearch] = useState('');
-  const [phone, setPhone] = useState('');
-  const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [search, setSearch] = useState('');
+    const [phone, setPhone] = useState('');
+    const [name, setName] = useState('');
 
-  return (
-    <Layout>
-      <Badge text="Components" />
-      <div className="flex flex-col mb-5">
-        <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Input</h1>
-        <p className="text-sm md:text-base font-normal font-sans text-gray-900 dark:text-gray-50">
-          A field where users can enter data or information.
-        </p>
-      </div>
-      {/* Variants Input */}
-      <section className="mb-5">
-        <h2 className="text-lg md:text-2xl font-medium font-sans text-gray-900 dark:text-gray-50 mb-5">Variants</h2>
-        <div className="space-y-4 flex flex-col justify-around">
-          {/* Email Input */}
-          <div>
-            <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3">
-              Default Input
-            </h3>
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email"
-              placeholder="name@email.com"
-              hint="This is a hint text."
-              className="max-w-md"
-            />
-          </div>
-          {/* Email Input with Icon */}
-          <div>
-            <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3">
-              Input with Icon
-            </h3>
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email with Icon"
-              placeholder="name@email.com"
-              hint="This is a hint text."
-              icon={<InformationCircleIcon className="h-5 w-5 text-gray-400" />}
-              className="max-w-md"
-            />
-          </div>
-          {/* Email Input with Error */}
-          <div>
-            <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3">
-              Input with Error
-            </h3>
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email with Error"
-              placeholder="name@email.com"
-              error="This is an error message."
-              className="max-w-md"
-            />
-          </div>
-          {/* Password Input */}
-          <div>
-            <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3">
-              Password Input
-            </h3>
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              label="Password"
-              placeholder="Enter your password"
-              className="max-w-md"
-            />
-          </div>
-          {/* Password Input with Icon */}
-          <div>
-            <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3">
-              Password Input with Icon (Toggle Visibility)
-            </h3>
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              label="Password with Icon"
-              placeholder="Enter your password"
-              icon={<LockClosedIcon className="h-5 w-5 text-gray-400" />}
-              className="max-w-md"
-            />
-          </div>
-          {/* Disabled Input */}
-          <div>
-            <h3 className="text-base md:text-lg font-medium font-sans text-gray-900 dark:text-gray-50 mb-2 md:mb-3">
-              Disabled Input
-            </h3>
-            <Input
-              type="text"
-              value="Disabled Input"
-              onChange={() => {}}
-              label="Disabled"
-              placeholder="This input is disabled"
-              className="max-w-md"
-              disabled
-            />
-          </div>
-        </div>
-      </section>
-      <InputDocumentation />
-    </Layout>
-  );
+    const tocItems = [
+        { id: 'overview', label: 'Overview' },
+        { id: 'usage', label: 'Usage' },
+        { id: 'properties', label: 'Properties' },
+        { id: 'best-practices', label: 'Best Practices' },
+    ];
+
+    const codeExample = `
+    <Input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+        placeholder="name@email.com"
+        hint="This is a hint text."
+        className="max-w-md"
+    />
+    <Input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        placeholder="Enter your password"
+        className="max-w-md"
+    />
+    `;
+
+    return (
+        <Layout>
+            <div className="flex">
+                <div className="w-4/5 pr-2">
+                    <Badge text="Components" />
+                    <div id="overview" className="flex flex-col mb-5 scroll-mt-16">
+                        <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-900 dark:text-gray-50">Input</h1>
+                        <p className="text-sm md:text-base font-normal font-sans text-gray-900 dark:text-gray-50">
+                            A field where users can enter data or information.
+                        </p>
+                    </div>
+
+                    {/* Usage Section */}
+                    <section id="usage" className="scroll-mt-20">
+                        <Tabs>
+                            <Tab label="Preview">
+                                <div className="relative w-full overflow-hidden mt-4 rounded-lg bg-zinc-100 dark:bg-zinc-950 shadow border border-slate-300 dark:border-slate-800">
+                                    <div className="p-6 space-y-4 flex flex-col justify-start items-start">
+                                        {/* Email Input */}
+                                        <div>
+                                            <h3 className="text-lg font-medium mb-4 text-gray-900 dark:text-gray-50">Default Input</h3>
+                                            <Input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                label="Email"
+                                                placeholder="name@email.com"
+                                                hint="This is a hint text."
+                                                className="max-w-md"
+                                            />
+                                        </div>
+                                        {/* Email Input with Icon */}
+                                        <div>
+                                            <h3 className="text-lg font-medium mb-4 text-gray-900 dark:text-gray-50">Input with Icon</h3>
+                                            <Input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                label="Email with Icon"
+                                                placeholder="name@email.com"
+                                                hint="This is a hint text."
+                                                icon={<InformationCircleIcon className="h-5 w-5 text-gray-400" />}
+                                                className="max-w-md"
+                                            />
+                                        </div>
+                                        {/* Email Input with Error */}
+                                        <div>
+                                            <h3 className="text-lg font-medium mb-4 text-gray-900 dark:text-gray-50">Input with Error</h3>
+                                            <Input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                label="Email with Error"
+                                                placeholder="name@email.com"
+                                                error="This is an error message."
+                                                className="max-w-md"
+                                            />
+                                        </div>
+                                        {/* Password Input */}
+                                        <div>
+                                            <h3 className="text-lg font-medium mb-4 text-gray-900 dark:text-gray-50">Password Input</h3>
+                                            <Input
+                                                type="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                label="Password"
+                                                placeholder="Enter your password"
+                                                className="max-w-md"
+                                            />
+                                        </div>
+                                        {/* Password Input with Icon */}
+                                        <div>
+                                            <h3 className="text-lg font-medium mb-4 text-gray-900 dark:text-gray-50">Password Input with Icon (Toggle Visibility)</h3>
+                                            <Input
+                                                type="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                                label="Password with Icon"
+                                                placeholder="Enter your password"
+                                                icon={<LockClosedIcon className="h-5 w-5 text-gray-400" />}
+                                                className="max-w-md"
+                                            />
+                                        </div>
+                                        {/* Disabled Input */}
+                                        <div>
+                                            <h3 className="text-lg font-medium mb-4 text-gray-900 dark:text-gray-50">Disabled Input</h3>
+                                            <Input
+                                                type="text"
+                                                value="Disabled Input"
+                                                onChange={() => {}}
+                                                label="Disabled"
+                                                placeholder="This input is disabled"
+                                                className="max-w-md"
+                                                disabled
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </Tab>
+                            <Tab label="Code">
+                                <div
+                                    className="relative rounded-lg bg-gray-900 dark:bg-gray-800/90 mt-4"
+                                    role="region"
+                                    aria-label="Code Snippet in tsx"
+                                >
+                                    <div className="flex justify-between items-center px-4 py-2 border-b border-gray-800">
+                                        <span className="text-sm font-medium text-gray-100">
+                                            tsx
+                                        </span>
+                                    </div>
+                                    <div className="overflow-auto rounded-lg border border-gray-800">
+                                        <SyntaxHighlighter
+                                            language="tsx"
+                                            style={materialLight}
+                                            showLineNumbers={false}
+                                            wrapLongLines={true}
+                                            customStyle={{
+                                                margin: 1.5,
+                                                borderRadius: '0.5rem 0.5rem 0.5rem 0.5rem',
+                                                fontSize: '14px',
+                                                lineHeight: '1.25',
+                                                padding: '10px',
+                                            }}
+                                        >
+                                            {codeExample}
+                                        </SyntaxHighlighter>
+                                    </div>
+                                </div>
+                            </Tab>
+                        </Tabs>
+                    </section>
+
+                    <InputDocumentation />
+                </div>
+                <TOC items={tocItems} />
+            </div>
+        </Layout>
+    );
 };
 
 InputPageClient.displayName = "InputPageClient";
