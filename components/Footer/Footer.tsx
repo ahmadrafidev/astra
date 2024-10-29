1e0f86681794071004be324038acd86dbaea04fe
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-10-29 14:41:38 +0700

    chore: change footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,10 +7,10 @@ const Footer: React.FC = () => {
     return (
         <footer className="flex flex-col sm:flex-row py-4 md:py-8 w-full items-center px-6 md:px-14 bg-gray-50 dark:bg-black justify-between" aria-label="Main Footer">
             <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300">
-                Engineered with passion by <Link href="https://x.com/rafiwiranaa" passHref className="underline" aria-label="Visit Rafi's Twitter">Rafi</Link>.
+                Built by <Link href="https://x.com/rafiwiranaa" passHref className="underline" aria-label="Visit Rafi's Twitter">Rafi</Link>.
             </p>
             <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300" aria-label="Copyright Information">
-                © 2024 AstraUI
+                © {new Date().getFullYear()} AstraUI
             </p>
         </footer>
     );
